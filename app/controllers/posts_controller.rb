c6013ce3343bbe1cd86fc47ff824a4bcfca8cf2d
nmhummel <natalie.hummel@gmail.com>
2021-01-12 08:30:18 -0600

    commit tree a66d05a15e7395cce9ad6d47df2c9ba6327e8fae

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
 
+  # def index
+  #   @posts = Post.all
+  # end
+
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
 
   def show
